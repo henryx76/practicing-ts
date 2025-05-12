f2d97dbc3aa403d8153d9ee4df91910b9439ded7
henryx76 <henryhesdras349@gmail.com>
2025-05-12 19:30:56 +0000

    subindo aula

---
+++ orientacao-objetos/src/interface.ts
@@ -0,0 +1,34 @@
+interface location = {
+    latitude: number;
+    longitude: number;
+}
+const getLocation = (location: location): string => {
+    return '${location.latitude}- ${location.longitude}'
+    
+}
+
+//const location = {
+//    latitude: 12312,
+//    longetude:321
+//}
+
+//getLocation(location)
+class Location implements Location {
+    latitude: number;
+    longitude: number;
+    numberLocation: number = 0;
+
+    constructor(latitude: number, longetude: number) {
+        this.latitude = latitude
+        this.longitude = longetude
+    }
+
+    getLocation(): string {
+        return 'any';
+    }
+}
+
+
+getLocation(new LocationMap(123,321))
+
+export{}
